f2bc3a0bba0635fd324ec89497daa0c3677ea4a1
DHK <bazzi95@naver.com>
2025-04-18 11:19:15 +0900

    1일차 정리

---
+++ day01/FirstCSharpApp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace FirstCSharpApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, C#!");
+            
+        }
+    }
+}
