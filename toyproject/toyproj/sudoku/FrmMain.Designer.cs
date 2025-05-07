f474361af549d9e8595768e1308c739e16f0e019
DHK <bazzi95@naver.com>
2025-05-07 16:51:49 +0900

    윈앱 미니 프로젝트
    스도쿠 게임

---
+++ toyproject/toyproj/sudoku/FrmMain.Designer.cs
@@ -0,0 +1,298 @@
+﻿
+namespace sudoku
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            BtnCheck = new System.Windows.Forms.Button();
+            BtnRestart = new System.Windows.Forms.Button();
+            label2 = new System.Windows.Forms.Label();
+            BtnAnswer = new System.Windows.Forms.Button();
+            label1 = new System.Windows.Forms.Label();
+            BtnSave = new System.Windows.Forms.Button();
+            BtnLoad = new System.Windows.Forms.Button();
+            groupBox1 = new System.Windows.Forms.GroupBox();
+            btnHintSec = new System.Windows.Forms.Button();
+            groupBox2 = new System.Windows.Forms.GroupBox();
+            lblTimer = new System.Windows.Forms.Label();
+            lblClearedGames = new System.Windows.Forms.Label();
+            lblNewGame = new System.Windows.Forms.Label();
+            lblCheckCount = new System.Windows.Forms.Label();
+            groupBox3 = new System.Windows.Forms.GroupBox();
+            label6 = new System.Windows.Forms.Label();
+            label5 = new System.Windows.Forms.Label();
+            label4 = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // BtnCheck
+            // 
+            BtnCheck.Location = new System.Drawing.Point(274, 60);
+            BtnCheck.Name = "BtnCheck";
+            BtnCheck.Size = new System.Drawing.Size(85, 30);
+            BtnCheck.TabIndex = 3;
+            BtnCheck.Text = "정답 확인";
+            BtnCheck.UseVisualStyleBackColor = true;
+            BtnCheck.Click += BtnCheck_Click;
+            // 
+            // BtnRestart
+            // 
+            BtnRestart.Location = new System.Drawing.Point(8, 22);
+            BtnRestart.Name = "BtnRestart";
+            BtnRestart.Size = new System.Drawing.Size(85, 30);
+            BtnRestart.TabIndex = 1;
+            BtnRestart.Text = "새 게임시작";
+            BtnRestart.UseVisualStyleBackColor = true;
+            BtnRestart.Click += BtnRestart_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(410, 9);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(31, 15);
+            label2.TabIndex = 12;
+            label2.Text = "힌트";
+            // 
+            // BtnAnswer
+            // 
+            BtnAnswer.Location = new System.Drawing.Point(274, 24);
+            BtnAnswer.Name = "BtnAnswer";
+            BtnAnswer.Size = new System.Drawing.Size(85, 30);
+            BtnAnswer.TabIndex = 2;
+            BtnAnswer.Text = "힌트 확인";
+            BtnAnswer.UseVisualStyleBackColor = true;
+            BtnAnswer.Click += BtnAnswer_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(43, 15);
+            label1.TabIndex = 12;
+            label1.Text = "게임판";
+            // 
+            // BtnSave
+            // 
+            BtnSave.Location = new System.Drawing.Point(6, 60);
+            BtnSave.Name = "BtnSave";
+            BtnSave.Size = new System.Drawing.Size(85, 30);
+            BtnSave.TabIndex = 4;
+            BtnSave.Text = "저장";
+            BtnSave.UseVisualStyleBackColor = true;
+            BtnSave.Click += BtnSave_Click;
+            // 
+            // BtnLoad
+            // 
+            BtnLoad.Location = new System.Drawing.Point(95, 60);
+            BtnLoad.Name = "BtnLoad";
+            BtnLoad.Size = new System.Drawing.Size(85, 30);
+            BtnLoad.TabIndex = 5;
+            BtnLoad.Text = "불러오기";
+            BtnLoad.UseVisualStyleBackColor = true;
+            BtnLoad.Click += BtnLoad_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(BtnSave);
+            groupBox1.Controls.Add(btnHintSec);
+            groupBox1.Controls.Add(BtnAnswer);
+            groupBox1.Controls.Add(BtnLoad);
+            groupBox1.Controls.Add(BtnCheck);
+            groupBox1.Controls.Add(BtnRestart);
+            groupBox1.Location = new System.Drawing.Point(12, 447);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new System.Drawing.Size(367, 102);
+            groupBox1.TabIndex = 13;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "메뉴";
+            // 
+            // btnHintSec
+            // 
+            btnHintSec.Enabled = false;
+            btnHintSec.Location = new System.Drawing.Point(184, 24);
+            btnHintSec.Name = "btnHintSec";
+            btnHintSec.Size = new System.Drawing.Size(85, 30);
+            btnHintSec.TabIndex = 2;
+            btnHintSec.Text = "3초 힌트";
+            btnHintSec.UseVisualStyleBackColor = true;
+            btnHintSec.Visible = false;
+            btnHintSec.Click += btnHintSec_Click;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(lblTimer);
+            groupBox2.Controls.Add(lblClearedGames);
+            groupBox2.Controls.Add(lblNewGame);
+            groupBox2.Controls.Add(lblCheckCount);
+            groupBox2.Location = new System.Drawing.Point(410, 447);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new System.Drawing.Size(175, 100);
+            groupBox2.TabIndex = 14;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "점수판";
+            // 
+            // lblTimer
+            // 
+            lblTimer.AutoSize = true;
+            lblTimer.Location = new System.Drawing.Point(27, 76);
+            lblTimer.Name = "lblTimer";
+            lblTimer.Size = new System.Drawing.Size(114, 15);
+            lblTimer.TabIndex = 3;
+            lblTimer.Text = "진행시간 : 00:00:00";
+            // 
+            // lblClearedGames
+            // 
+            lblClearedGames.AutoSize = true;
+            lblClearedGames.Location = new System.Drawing.Point(11, 58);
+            lblClearedGames.Name = "lblClearedGames";
+            lblClearedGames.Size = new System.Drawing.Size(89, 15);
+            lblClearedGames.TabIndex = 2;
+            lblClearedGames.Text = "완료게임 수 : 0";
+            // 
+            // lblNewGame
+            // 
+            lblNewGame.AutoSize = true;
+            lblNewGame.Location = new System.Drawing.Point(11, 19);
+            lblNewGame.Name = "lblNewGame";
+            lblNewGame.Size = new System.Drawing.Size(89, 15);
+            lblNewGame.TabIndex = 1;
+            lblNewGame.Text = "게임시작 수 : 0";
+            // 
+            // lblCheckCount
+            // 
+            lblCheckCount.AutoSize = true;
+            lblCheckCount.Location = new System.Drawing.Point(11, 39);
+            lblCheckCount.Name = "lblCheckCount";
+            lblCheckCount.Size = new System.Drawing.Size(89, 15);
+            lblCheckCount.TabIndex = 0;
+            lblCheckCount.Text = "정답확인 수 : 0";
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(label6);
+            groupBox3.Controls.Add(label5);
+            groupBox3.Controls.Add(label4);
+            groupBox3.Controls.Add(label3);
+            groupBox3.Location = new System.Drawing.Point(598, 447);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new System.Drawing.Size(175, 100);
+            groupBox3.TabIndex = 15;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "게임방법";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new System.Drawing.Point(18, 76);
+            label6.Name = "label6";
+            label6.Size = new System.Drawing.Size(122, 15);
+            label6.TabIndex = 0;
+            label6.Text = "스페이스 바 : 값 증가";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new System.Drawing.Point(6, 58);
+            label5.Name = "label5";
+            label5.Size = new System.Drawing.Size(134, 15);
+            label5.TabIndex = 0;
+            label5.Text = "키보드 방향키 : 칸 이동";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new System.Drawing.Point(6, 39);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(134, 15);
+            label4.TabIndex = 0;
+            label4.Text = "마우스 우클릭 : 값 감소";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new System.Drawing.Point(6, 19);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(134, 15);
+            label3.TabIndex = 0;
+            label3.Text = "마우스 좌클릭 : 값 증가";
+            // 
+            // FrmMain
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(784, 561);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(label1);
+            Controls.Add(label2);
+            Icon = (System.Drawing.Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "FrmMain";
+            Text = "스도쿠";
+            Load += FrmMain_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button BtnCheck;
+        private System.Windows.Forms.Button BtnRestart;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BtnAnswer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtnSave;
+        private System.Windows.Forms.Button BtnLoad;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblCheckCount;
+        private System.Windows.Forms.Label lblNewGame;
+        private System.Windows.Forms.Label lblClearedGames;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnHintSec;
+        private System.Windows.Forms.Label lblTimer;
+    }
+}
+
---
+++ toyproject/toyproj/sudoku/FrmMain.cs
@@ -0,0 +1,399 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
+namespace sudoku
+{
+    public partial class FrmMain : Form
+    {
+        Button[,] buttons = new Button[9, 9];
+        Button[,] answerButtons = new Button[9, 9];
+        int[,] board = new int[9, 9];   // 정답 보드
+        int[,] puzzle = new int[9, 9];  // 퍼즐 보드
+        int[,] hintMap = new int[9, 9]; // 0: 사용자 입력 가능, 1: 힌트로 고정
+        Random rand = new Random();
+        bool isAnswerShown = false;
+        int checkAnswerCount = 0;
+        int newGameCount = 0;
+        int clearedGameCount = 0;
+        string saveFilePath = "save.txt";
+        Timer hintTimer = new Timer();
+        Timer gameTimer = new Timer();
+        int elapsedSeconds = 0;
+
+        public FrmMain()
+        {
+            InitializeComponent();
+            CreateGrid();
+            StartNewGame();
+
+            // 힌트 타이머 설정
+            hintTimer.Interval = 3000; // 3초
+            hintTimer.Tick += HintTimer_Tick;
+
+            // 진행시간 타이머 설정
+            gameTimer.Interval = 1000; // 1초
+            gameTimer.Tick += GameTimer_Tick;
+        }
+
+        void CreateGrid()
+        {
+            int size = 40;
+            int spacing = 40;
+
+            int totalWidth = size * 9 * 2 + spacing;
+            int startX = (this.ClientSize.Width - totalWidth) / 2;
+            int startY = (this.ClientSize.Height - size * 9) / 2 - 50;
+
+            for (int row = 0; row < 9; row++)
+            {
+                for (int col = 0; col < 9; col++)
+                {
+                    Button btnPuzzle = new Button();
+                    btnPuzzle.Width = size;
+                    btnPuzzle.Height = size;
+                    btnPuzzle.Left = startX + col * size;
+                    btnPuzzle.Top = startY + row * size;
+                    btnPuzzle.Font = new Font("Arial", 14);
+                    btnPuzzle.Tag = (row, col);
+                    btnPuzzle.Click += Btn_Click;
+                    btnPuzzle.MouseDown += Btn_MouseDown;
+                    this.Controls.Add(btnPuzzle);
+                    buttons[row, col] = btnPuzzle;
+
+                    Button btnAnswer = new Button();
+                    btnAnswer.Width = size;
+                    btnAnswer.Height = size;
+                    btnAnswer.Left = startX + size * 9 + spacing + col * size;
+                    btnAnswer.Top = startY + row * size;
+                    btnAnswer.Font = new Font("Arial", 14);
+                    btnAnswer.Enabled = false;
+                    btnAnswer.BackColor = Color.LightGray;
+                    btnAnswer.FlatStyle = FlatStyle.Flat;
+                    btnAnswer.FlatAppearance.BorderSize = 1;
+                    btnAnswer.FlatAppearance.BorderColor = Color.DarkGray;
+                    this.Controls.Add(btnAnswer);
+                    answerButtons[row, col] = btnAnswer;
+                }
+            }
+        }
+
+        void Btn_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            var (row, col) = ((int, int))btn.Tag;
+
+            if (puzzle[row, col] == 0 || hintMap[row, col] == 0)
+            {
+                int current = string.IsNullOrEmpty(btn.Text) ? 0 : int.Parse(btn.Text);
+                int next = (current % 9) + 1;
+                btn.Text = next.ToString();
+            }
+        }
+
+        void Btn_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                Button btn = sender as Button;
+                var (row, col) = ((int, int))btn.Tag;
+
+                if (puzzle[row, col] == 0 || hintMap[row, col] == 0)
+                {
+                    int current = string.IsNullOrEmpty(btn.Text) ? 10 : int.Parse(btn.Text);
+                    int next = (current == 1) ? 9 : (current - 1);
+                    btn.Text = next.ToString();
+                    puzzle[row, col] = next;
+                    hintMap[row, col] = 0;
+                }
+            }
+        }
+
+
+        void StartNewGame()
+        {
+            Array.Clear(board, 0, board.Length);
+            Array.Clear(hintMap, 0, hintMap.Length);
+            FillBoard(board);
+            Array.Copy(board, puzzle, board.Length);
+            RemoveNumbers(puzzle, 40);
+            UpdateHintMap();
+            UpdateGrid();
+            elapsedSeconds = 0; // ⬅️ 시간 초기화
+            gameTimer.Start();
+        }
+
+        void UpdateHintMap()
+        {
+            for (int r = 0; r < 9; r++)
+            {
+                for (int c = 0; c < 9; c++)
+                {
+                    hintMap[r, c] = puzzle[r, c] == 0 ? 0 : 1;
+                }
+            }
+        }
+
+        void UpdateGrid()
+        {
+            for (int row = 0; row < 9; row++)
+            {
+                for (int col = 0; col < 9; col++)
+                {
+                    int val = puzzle[row, col];
+                    buttons[row, col].Text = val == 0 ? "" : val.ToString();
+
+                    if (hintMap[row, col] == 0)
+                    {
+                        buttons[row, col].Enabled = true;
+                        buttons[row, col].BackColor = Color.White;
+                        buttons[row, col].ForeColor = Color.Black;
+                    }
+                    else
+                    {
+                        buttons[row, col].Enabled = false;
+                        buttons[row, col].BackColor = Color.LightGray;
+                        buttons[row, col].ForeColor = Color.DarkBlue;
+                    }
+                }
+            }
+        }
+
+        bool FillBoard(int[,] board, int row = 0, int col = 0)
+        {
+            if (row == 9) return true;
+            if (col == 9) return FillBoard(board, row + 1, 0);
+
+            var numbers = Enumerable.Range(1, 9).OrderBy(n => rand.Next()).ToList();
+            foreach (int num in numbers)
+            {
+                if (IsValid(board, row, col, num))
+                {
+                    board[row, col] = num;
+                    if (FillBoard(board, row, col + 1))
+                        return true;
+                    board[row, col] = 0;
+                }
+            }
+            return false;
+        }
+
+        bool IsValid(int[,] board, int row, int col, int num)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                if (board[row, i] == num || board[i, col] == num)
+                    return false;
+            }
+
+            int startRow = row / 3 * 3;
+            int startCol = col / 3 * 3;
+            for (int i = startRow; i < startRow + 3; i++)
+                for (int j = startCol; j < startCol + 3; j++)
+                    if (board[i, j] == num)
+                        return false;
+
+            return true;
+        }
+
+        void RemoveNumbers(int[,] puzzle, int removeCount)
+        {
+            int removed = 0;
+            while (removed < removeCount)
+            {
+                int row = rand.Next(9);
+                int col = rand.Next(9);
+                if (puzzle[row, col] != 0)
+                {
+                    puzzle[row, col] = 0;
+                    removed++;
+                }
+            }
+        }
+
+        void ShowAnswer()
+        {
+            for (int row = 0; row < 9; row++)
+            {
+                for (int col = 0; col < 9; col++)
+                {
+                    answerButtons[row, col].Text = board[row, col].ToString();
+                    answerButtons[row, col].BackColor = puzzle[row, col] == 0 ? Color.LightBlue : Color.LightGray;
+                    answerButtons[row, col].ForeColor = puzzle[row, col] == 0 ? Color.Black : Color.DarkBlue;
+                }
+            }
+        }
+
+        void HideAnswer()
+        {
+            for (int row = 0; row < 9; row++)
+            {
+                for (int col = 0; col < 9; col++)
+                {
+                    answerButtons[row, col].Text = "";
+                    answerButtons[row, col].Enabled = false;
+                    answerButtons[row, col].BackColor = Color.LightGray;
+                    answerButtons[row, col].ForeColor = Color.DarkGray;
+                }
+            }
+        }
+
+        private void BtnAnswer_Click(object sender, EventArgs e)
+        {
+            if (!isAnswerShown)
+            {
+                ShowAnswer();
+                isAnswerShown = true;
+                ((Button)sender).Text = "힌트 숨기기";
+            }
+            else
+            {
+                HideAnswer();
+                isAnswerShown = false;
+                ((Button)sender).Text = "힌트 확인";
+            }
+        }
+
+        bool IsCorrect()
+        {
+            for (int row = 0; row < 9; row++)
+                for (int col = 0; col < 9; col++)
+                {
+                    int input = string.IsNullOrEmpty(buttons[row, col].Text) ? 0 : int.Parse(buttons[row, col].Text);
+                    if (input != board[row, col])
+                        return false;
+                }
+            return true;
+        }
+
+        private void BtnCheck_Click(object sender, EventArgs e)
+        {
+            if (IsCorrect())
+            {
+                MessageBox.Show("정답입니다!", "정답 확인!");
+                lblClearedGames.Text = $"완료게임 수 : {clearedGameCount}";
+            }
+            else
+            {
+                MessageBox.Show("틀렸습니다. 다시 풀어보세요~", "정답 확인!");
+            }
+            checkAnswerCount++;
+            lblCheckCount.Text = $"정답확인 수 : {checkAnswerCount}";
+        }
+
+        private void BtnRestart_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("정말 새로 시작하시겠습니까?", "새 게임 시작", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                StartNewGame();
+                HideAnswer();
+                isAnswerShown = false;
+                BtnAnswer.Text = "힌트 확인";
+                newGameCount++;
+                lblNewGame.Text = $"게임시작 수 : {newGameCount}";
+            }
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("저장하시겠습니까?", "게임 저장", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                using (StreamWriter sw = new StreamWriter(saveFilePath))
+                {
+                    for (int r = 0; r < 9; r++)
+                        sw.WriteLine(string.Join(",", Enumerable.Range(0, 9).Select(c => string.IsNullOrEmpty(buttons[r, c].Text) ? "0" : buttons[r, c].Text)));
+
+                    for (int r = 0; r < 9; r++)
+                        sw.WriteLine(string.Join(",", Enumerable.Range(0, 9).Select(c => hintMap[r, c])));
+
+                    for (int r = 0; r < 9; r++)
+                        sw.WriteLine(string.Join(",", Enumerable.Range(0, 9).Select(c => board[r, c])));
+
+                    sw.WriteLine(checkAnswerCount);
+                    sw.WriteLine(newGameCount);
+                    sw.WriteLine(elapsedSeconds); // 시간 저장
+                }
+
+                MessageBox.Show("저장 완료!", "게임 저장");
+            }
+        }
+
+        private void BtnLoad_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("불러오시겠습니까?", "게임 불러오기", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                if (!File.Exists(saveFilePath))
+                {
+                    MessageBox.Show("저장된 파일이 없습니다.");
+                    return;
+                }
+
+                using (StreamReader sr = new StreamReader(saveFilePath))
+                {
+                    for (int r = 0; r < 9; r++)
+                    {
+                        var vals = sr.ReadLine().Split(',').Select(int.Parse).ToArray();
+                        for (int c = 0; c < 9; c++)
+                        {
+                            puzzle[r, c] = vals[c];
+                            buttons[r, c].Text = vals[c] == 0 ? "" : vals[c].ToString();
+                        }
+                    }
+
+                    for (int r = 0; r < 9; r++)
+                    {
+                        var hints = sr.ReadLine().Split(',').Select(int.Parse).ToArray();
+                        for (int c = 0; c < 9; c++) hintMap[r, c] = hints[c];
+                    }
+
+                    for (int r = 0; r < 9; r++)
+                    {
+                        var ans = sr.ReadLine().Split(',').Select(int.Parse).ToArray();
+                        for (int c = 0; c < 9; c++) board[r, c] = ans[c];
+                    }
+
+                    checkAnswerCount = int.Parse(sr.ReadLine());
+                    newGameCount = int.Parse(sr.ReadLine());
+
+                    lblCheckCount.Text = $"정답확인 수 : {checkAnswerCount}";
+                    lblNewGame.Text = $"게임시작 수 : {newGameCount}";
+                    UpdateGrid();
+                    elapsedSeconds = int.Parse(sr.ReadLine());
+                    TimeSpan time = TimeSpan.FromSeconds(elapsedSeconds);
+                    lblTimer.Text = $"진행시간: {time:hh\\:mm\\:ss}";
+                }
+
+                MessageBox.Show("불러오기 완료!", "게임 불러오기");
+            }
+        }
+        private void HintTimer_Tick(object sender, EventArgs e)
+        {
+            HideAnswer();
+            isAnswerShown = false;
+            BtnAnswer.Text = "힌트 확인";
+            hintTimer.Stop(); // 타이머 종료
+        }
+        private void btnHintSec_Click(object sender, EventArgs e)
+        {
+            if (!isAnswerShown)
+            {
+                ShowAnswer();
+                hintTimer.Start(); // 3초 후 자동 숨김 시작
+            }
+    
+        }
+
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+            elapsedSeconds++;
+            TimeSpan time = TimeSpan.FromSeconds(elapsedSeconds);
+            lblTimer.Text = $"진행시간 : {time:hh\\:mm\\:ss}";
+        }
+        private void FrmMain_Load(object sender, EventArgs e) { }
+
+    }
+}
